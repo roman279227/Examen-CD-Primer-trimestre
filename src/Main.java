cc45b3ccbd4cb0ffa299c7efabd5d670d1bed8d7
ATEIS\a22danielfr <danielfernandezroman27@gmail.com>
2022-12-05 17:38:02 +0100

    modified:   .idea/workspace.xml 	modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,36 @@
-package com.empresa.tutorial_java.paso_valor.paso_objetos;
+package com.empresa.tutorial_java.paso_valor.paso_arrays;
 
-class Clase {
-    int entero;
-    String string;
+import java.util.Arrays;
 
-    public Clase(int i, String string) {
-        this.entero = i;
-        this.string = string;
+class PasoArrays {
+
+    public static void main(String[] args) {
+
+        int[] array1 = new int[5];
+        printArray(array1);
+        array1[0] = 0;  // innecesario ya que inicializa a 0 por defecto
+
+        int[] array2 = array1;  // dos variables apuntando al mismo array
+        int[] array3 = Arrays.copyOf(array1, array1.length); // dos arrays iguales
+
+        IncrementaArray(array1);
+        IncrementaArray(array2);
+        IncrementaArray(array3);
+
+        printArray(array1);
+        printArray(array2);
+        printArray(array3);
     }
 
-    @Override
-    public String toString() {
-        return "Objeto { i=" + entero + ", a='" + string + "'}";
+    private static void IncrementaArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i] + 1;
+        }
     }
+
+    private static void printArray(int[] array) {
+        Arrays.stream(array).forEach(System.out::print);
+        System.out.println();
+    }
+
 }
