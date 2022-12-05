964040c58357d5b2381b008ae3bbe80a64912912
ATEIS\a22danielfr <danielfernandezroman27@gmail.com>
2022-12-05 18:17:13 +0100

    modified:   .idea/workspace.xml 	modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,124 @@
-package com.empresa.tutorial_java.paso_valor.paso_objetos;
-
-class PasoObjetos {
-
-    public static void main(String[] args) {
-
-        System.out.println("\n--- Pasando un int");
-        int entero = 1;
-        modificaVarInt(entero);  // pasamos el valor del entero
-        System.out.println("Valor del int fuera del método:    " + entero);
-
-        System.out.println("\n--- Pasando un String");
-        String string = "string1";
-        modificaVarString(string); // pasamos el valor del String
-        System.out.println("Valor del string fuera del método:  " + string);
-
-        System.out.println("\n--- Pasando un objeto y reasignando el parámetro");
-        Clase objeto = new Clase(1, "string1");
-        modificaVarObjeto(objeto); // pasamos la referencia al objeto
-        System.out.println("Valor del objeto fuera del método: " + objeto);
-
-        System.out.println("\n--- Pasando un objeto y modificando su contenido");
-        Clase objeto2 = new Clase(1, "string1");
-        modificaContenidoObjeto(objeto2); // pasamos la referencia al objeto
-        System.out.println("Valor del objeto fuera del método: " + objeto2);
-
-    }
-
-
-    private static void modificaVarInt(int a) {
-        a++; // Incrementamos en 1
-        System.out.println("Valor del int dentro del método:   " + a);
-    }
-
-    private static void modificaVarString(String a) {
-        a += " modificado";
-        System.out.println("Valor del string dentro del método: " + a);
-    }
-
-    private static void modificaVarObjeto(Clase o) {
-        o = new Clase(o.entero + 1, "nuevo string"); // creamos un nueva instancia
-        System.out.println("Valor del objeto dentro de método: " + o);
-    }
-
-    private static void modificaContenidoObjeto(Clase o) {
-        o.entero = -99; // modificamos el contenido de la referencia
-        o.string = "string modificado";
-        System.out.println("Valor del objeto dentro de método: " + o);
-    }
-
-}
+<?xml version="1.0" encoding="UTF-8"?>
+<project version="4">
+<component name="Palette2">
+<group name="Swing">
+<item class="com.intellij.uiDesigner.HSpacer" tooltip-text="Horizontal Spacer" icon="/com/intellij/uiDesigner/icons/hspacer.svg" removable="false" auto-create-binding="false" can-attach-label="false">
+<default-constraints vsize-policy="1" hsize-policy="6" anchor="0" fill="1" />
+</item>
+<item class="com.intellij.uiDesigner.VSpacer" tooltip-text="Vertical Spacer" icon="/com/intellij/uiDesigner/icons/vspacer.svg" removable="false" auto-create-binding="false" can-attach-label="false">
+<default-constraints vsize-policy="6" hsize-policy="1" anchor="0" fill="2" />
+</item>
+<item class="javax.swing.JPanel" icon="/com/intellij/uiDesigner/icons/panel.svg" removable="false" auto-create-binding="false" can-attach-label="false">
+<default-constraints vsize-policy="3" hsize-policy="3" anchor="0" fill="3" />
+</item>
+<item class="javax.swing.JScrollPane" icon="/com/intellij/uiDesigner/icons/scrollPane.svg" removable="false" auto-create-binding="false" can-attach-label="true">
+<default-constraints vsize-policy="7" hsize-policy="7" anchor="0" fill="3" />
+</item>
+<item class="javax.swing.JButton" icon="/com/intellij/uiDesigner/icons/button.svg" removable="false" auto-create-binding="true" can-attach-label="false">
+<default-constraints vsize-policy="0" hsize-policy="3" anchor="0" fill="1" />
+<initial-values>
+<property name="text" value="Button" />
+</initial-values>
+</item>
+<item class="javax.swing.JRadioButton" icon="/com/intellij/uiDesigner/icons/radioButton.svg" removable="false" auto-create-binding="true" can-attach-label="false">
+<default-constraints vsize-policy="0" hsize-policy="3" anchor="8" fill="0" />
+<initial-values>
+<property name="text" value="RadioButton" />
+</initial-values>
+</item>
+<item class="javax.swing.JCheckBox" icon="/com/intellij/uiDesigner/icons/checkBox.svg" removable="false" auto-create-binding="true" can-attach-label="false">
+<default-constraints vsize-policy="0" hsize-policy="3" anchor="8" fill="0" />
+<initial-values>
+<property name="text" value="CheckBox" />
+</initial-values>
+</item>
+<item class="javax.swing.JLabel" icon="/com/intellij/uiDesigner/icons/label.svg" removable="false" auto-create-binding="false" can-attach-label="false">
+<default-constraints vsize-policy="0" hsize-policy="0" anchor="8" fill="0" />
+<initial-values>
+<property name="text" value="Label" />
+</initial-values>
+</item>
+<item class="javax.swing.JTextField" icon="/com/intellij/uiDesigner/icons/textField.svg" removable="false" auto-create-binding="true" can-attach-label="true">
+<default-constraints vsize-policy="0" hsize-policy="6" anchor="8" fill="1">
+<preferred-size width="150" height="-1" />
+</default-constraints>
+</item>
+<item class="javax.swing.JPasswordField" icon="/com/intellij/uiDesigner/icons/passwordField.svg" removable="false" auto-create-binding="true" can-attach-label="true">
+<default-constraints vsize-policy="0" hsize-policy="6" anchor="8" fill="1">
+<preferred-size width="150" height="-1" />
+</default-constraints>
+</item>
+<item class="javax.swing.JFormattedTextField" icon="/com/intellij/uiDesigner/icons/formattedTextField.svg" removable="false" auto-create-binding="true" can-attach-label="true">
+<default-constraints vsize-policy="0" hsize-policy="6" anchor="8" fill="1">
+<preferred-size width="150" height="-1" />
+</default-constraints>
+</item>
+<item class="javax.swing.JTextArea" icon="/com/intellij/uiDesigner/icons/textArea.svg" removable="false" auto-create-binding="true" can-attach-label="true">
+<default-constraints vsize-policy="6" hsize-policy="6" anchor="0" fill="3">
+<preferred-size width="150" height="50" />
+</default-constraints>
+</item>
+<item class="javax.swing.JTextPane" icon="/com/intellij/uiDesigner/icons/textPane.svg" removable="false" auto-create-binding="true" can-attach-label="true">
+<default-constraints vsize-policy="6" hsize-policy="6" anchor="0" fill="3">
+<preferred-size width="150" height="50" />
+</default-constraints>
+</item>
+<item class="javax.swing.JEditorPane" icon="/com/intellij/uiDesigner/icons/editorPane.svg" removable="false" auto-create-binding="true" can-attach-label="true">
+<default-constraints vsize-policy="6" hsize-policy="6" anchor="0" fill="3">
+<preferred-size width="150" height="50" />
+</default-constraints>
+</item>
+<item class="javax.swing.JComboBox" icon="/com/intellij/uiDesigner/icons/comboBox.svg" removable="false" auto-create-binding="true" can-attach-label="true">
+<default-constraints vsize-policy="0" hsize-policy="2" anchor="8" fill="1" />
+</item>
+<item class="javax.swing.JTable" icon="/com/intellij/uiDesigner/icons/table.svg" removable="false" auto-create-binding="true" can-attach-label="false">
+<default-constraints vsize-policy="6" hsize-policy="6" anchor="0" fill="3">
+<preferred-size width="150" height="50" />
+</default-constraints>
+</item>
+<item class="javax.swing.JList" icon="/com/intellij/uiDesigner/icons/list.svg" removable="false" auto-create-binding="true" can-attach-label="false">
+<default-constraints vsize-policy="6" hsize-policy="2" anchor="0" fill="3">
+<preferred-size width="150" height="50" />
+</default-constraints>
+</item>
+<item class="javax.swing.JTree" icon="/com/intellij/uiDesigner/icons/tree.svg" removable="false" auto-create-binding="true" can-attach-label="false">
+<default-constraints vsize-policy="6" hsize-policy="6" anchor="0" fill="3">
+<preferred-size width="150" height="50" />
+</default-constraints>
+</item>
+<item class="javax.swing.JTabbedPane" icon="/com/intellij/uiDesigner/icons/tabbedPane.svg" removable="false" auto-create-binding="true" can-attach-label="false">
+<default-constraints vsize-policy="3" hsize-policy="3" anchor="0" fill="3">
+<preferred-size width="200" height="200" />
+</default-constraints>
+</item>
+<item class="javax.swing.JSplitPane" icon="/com/intellij/uiDesigner/icons/splitPane.svg" removable="false" auto-create-binding="false" can-attach-label="false">
+<default-constraints vsize-policy="3" hsize-policy="3" anchor="0" fill="3">
+<preferred-size width="200" height="200" />
+</default-constraints>
+</item>
+<item class="javax.swing.JSpinner" icon="/com/intellij/uiDesigner/icons/spinner.svg" removable="false" auto-create-binding="true" can-attach-label="true">
+<default-constraints vsize-policy="0" hsize-policy="6" anchor="8" fill="1" />
+</item>
+<item class="javax.swing.JSlider" icon="/com/intellij/uiDesigner/icons/slider.svg" removable="false" auto-create-binding="true" can-attach-label="false">
+<default-constraints vsize-policy="0" hsize-policy="6" anchor="8" fill="1" />
+</item>
+<item class="javax.swing.JSeparator" icon="/com/intellij/uiDesigner/icons/separator.svg" removable="false" auto-create-binding="false" can-attach-label="false">
+<default-constraints vsize-policy="6" hsize-policy="6" anchor="0" fill="3" />
+</item>
+<item class="javax.swing.JProgressBar" icon="/com/intellij/uiDesigner/icons/progressbar.svg" removable="false" auto-create-binding="true" can-attach-label="false">
+<default-constraints vsize-policy="0" hsize-policy="6" anchor="0" fill="1" />
+</item>
+<item class="javax.swing.JToolBar" icon="/com/intellij/uiDesigner/icons/toolbar.svg" removable="false" auto-create-binding="false" can-attach-label="false">
+<default-constraints vsize-policy="0" hsize-policy="6" anchor="0" fill="1">
+<preferred-size width="-1" height="20" />
+</default-constraints>
+</item>
+<item class="javax.swing.JToolBar$Separator" icon="/com/intellij/uiDesigner/icons/toolbarSeparator.svg" removable="false" auto-create-binding="false" can-attach-label="false">
+<default-constraints vsize-policy="0" hsize-policy="0" anchor="0" fill="1" />
+</item>
+<item class="javax.swing.JScrollBar" icon="/com/intellij/uiDesigner/icons/scrollbar.svg" removable="false" auto-create-binding="true" can-attach-label="false">
+<default-constraints vsize-policy="6" hsize-policy="0" anchor="0" fill="2" />
+</item>
+</group>
+</component>
+</project>
